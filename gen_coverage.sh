119098df39f0b707106bc9d0a926cf301ce475cb
CapsAdmin <eliashogstvedt@gmail.com>
2021-12-04 08:39:36 +0100

    make use of gcov vscode extension and cleanup camkefile

--- gen_coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf coverage
-mkdir coverage
-pushd build/CMakeFiles/tests.dir/tests/
-    lcov --capture --directory ../../../../ --output-file ../../../../coverage/lcov_output.info
-popd
-genhtml coverage/lcov_output.info --output-directory coverage/
